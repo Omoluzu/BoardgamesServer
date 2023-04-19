3b714f70da559a203a1363b9e7bdeac418c810c0
Aleksey Volkov <seishikage@gmail.com>
2023-04-19 18:20:30 +0300

    ignis - Стартовая конфигурация на сервере

--- modules/GamesApproved.py
+++ modules/GamesApproved.py
@@ -14,13 +14,13 @@ from src.games import start_game
 
 
 def approved_games(data):
-    print(data)
+    # print(data)
 
     game_info = ListGames.get_games_info(games_id=data['game_id'])
     started_configure = start_game.get(game_info['games'])
-    game_data = started_configure(games_config=game_info['games_config'])
+    game_data = started_configure(games_config=game_info)
 
-    print(game_data)
+    # print(game_data)
 
     # path_games = ListGames.get_path_games(id_games=data['game_id'])
 
--- src/games/ignis/view/started.py
+++ src/games/ignis/view/started.py
@@ -1,11 +1,34 @@
+import random
+
+
 def started_configure(games_config: dict) -> dict:
     """
     Создание стартовой конфигурации для игры ИГНИС
 
     games_config: Данный пришедшие с сервера.
     """
+    field = [
+        ["F", "F", "", "", "W", "W"],
+        ["F", "", "", "", "", "W"],
+        ["", "", "F", "W", "", ""],
+        ["", "", "W", "F", "", ""],
+        ["W", "", "", "", "", "F"],
+        ["W", "W", "", "", "F", "F"]
+    ]
+
+    select_type = random.randint(0, 1)
+
+    data = {
+        "field": field,
+        "count": "F8W8",  # F - fire, 8 - кол-во тайлов F на поле, W - water, 8 - кол-во тайлов W на поле
+        "active_player": games_config['users'][random.randint(0, 1)],
+        "kind": {
+            "F": games_config['users'][select_type],
+            "W": games_config['users'][1 - select_type],
+        },
+    }
 
     return {
         "server": "",
-        "players": ""
+        "players": data,
     }
